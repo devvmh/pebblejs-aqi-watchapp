91bc1dbf9f51c53e7fee8b88c8ef98a71a15ed9f
Devin Howard <devin@callysto.com>
2016-01-17 18:41:16 +0800

    v1.0 works

--- src/app.js
+++ src/app.js
@@ -1,62 +1,50 @@
 /**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
+ * AQI pebble app
+ * Author: Devin Howard
+ * All credits to http://aqicn.org!
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'AQICN',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
+  subtitle: '---',
+  body: 'Press select to download data.',
   bodyColor: '#9a0036' // Hex colors
 });
 
+var AQICN = {
+  aqi: {},
+  cityname: 'beijing',  
+  lang: null,
+  n: 0,
+  k: '',
+  success: function(data) {
+    this.n += 1;
+    var json = data.replace(/^.*{"cityname"/, '{"cityname"');
+    json = json.replace(/\)\);$/, ''); //remove )); from end
+    json = json.replace(/<[^>]*>/g, ''); //remove html tags
+    this.aqi = JSON.parse(json);
+    main.subtitle(this.aqi.cityname + ' ' + this.aqi.aqit);
+    main.body(this.aqi.attribution);
+    console.log(JSON.stringify(this.aqi, null, 1));
+  },
+  error: function(err) {
+    console.log('Ajax failed: ' + err);
+  },
+  getFeed: function() {
+    var url = 'http://feed.aqicn.org/feed/' + this.cityname + '/' + (this.lang || '') + '/feed.v1.js?n=' + this.n + this.k;
+    ajax({url: url}, this.success, this.error);
+  }//getFeed
+};//AQICN
+
+/*
+ * MAIN
+ */
+
 main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
 main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+  AQICN.getFeed();
 });
